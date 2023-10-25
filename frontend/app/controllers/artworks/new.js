9b00703d6e51d87f13113c885cead2b82708363c
Ruben Bottu <ruben.bottu@qmino.com>
2023-10-25 11:01:38 +0200

    Added form validation

--- frontend/app/controllers/artworks/new.js
+++ frontend/app/controllers/artworks/new.js
@@ -3,37 +3,87 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
+const allowedTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/webp'];
+
 export default class ArtworksNewController extends Controller {
-    @tracked newTitle = '';
-    @tracked newArtType = '';
-    @tracked newDescription = '';
-    @tracked newArtistName = '';
+  @tracked title = '';
+  @tracked artistName = '';
+  @tracked artType = '';
+  @tracked description = '';
 
-    @service store;
+  @tracked statusMessage = '';
+  @tracked titleError = '';
+  @tracked artistNameError = '';
+  @tracked artTypeError = '';
+  @tracked descriptionError = '';
+  @tracked dropzoneError = '';
 
-    @action
-    async createArtwork(event) {
-      event.preventDefault();
+  @tracked artImage = '';
+  @tracked preview = '';
 
-      const artist = this.store.createRecord('artist', {
-        name: this.newArtistName,
-      });
+  @service store;
 
-      await artist.save();
+  clearErrors() {
+    this.statusMessage = '';
+    this.titleError = '';
+    this.artistNameError = '';
+    this.artTypeError = '';
+    this.descriptionError = '';
+    this.dropzoneError = '';
+  }
 
-      const artwork = this.store.createRecord('artwork', {
-        title: this.newTitle,
-        artType: this.newArtType,
-        description: this.newDescription,
-        artist: artist,
-      });
+  isValidTextInput(text) {
+    return text && text.trim() !== '';
+  }
 
-      artwork.save();
+  validate() {
+    let result = true;
 
-      // clear the input fields
-      this.newTitle = '';
-      this.newArtType = '';
-      this.newDescription = '';
-      this.newArtistName = '';
+    if (!this.title || this.title.trim() === '') {
+      this.titleError = `Title required`;
+      result = false;
     }
+
+    if (!this.artistName || this.artistName.trim() === '') {
+      this.artistNameError = `Artist name is required`;
+      result = false;
+    }
+
+    if (!this.artType || this.artType.trim() === '') {
+      this.artTypeError = `Artform is required`;
+      result = false;
+    }
+
+    if (!this.description || this.description.trim() === '') {
+      this.descriptionError = `Description is required`;
+      result = false;
+    }
+
+    return result;
+  }
+
+  @action
+  async createArtwork(event) {
+    event.preventDefault();
+    this.clearErrors();
+
+    if (!this.validate()) {
+      return;
+    }
+
+    const artist = this.store.createRecord('artist', {
+      name: this.artistName,
+    });
+    await artist.save();
+
+    const artwork = this.store.createRecord('artwork', {
+      title: this.title,
+      artType: this.artType,
+      description: this.description,
+      artist: artist,
+    });
+    artwork.save();
+
+    this.statusMessage = `Creation successful`;
+  }
 }
--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -2,15 +2,15 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-    @service store;
+  @service store;
 
-    model() {
-        return this.store.findAll('artwork');
-    }
+  /* model() {
+    return this.store.findAll('artwork');
+  } */
 
-    /* model() {
-        return this.store.query('artwork', {
-            include: 'artist'
-        });
-    } */
+  model() {
+    return this.store.query('artwork', {
+      include: 'artist',
+    });
+  }
 }
