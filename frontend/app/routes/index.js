a7e2622f36fb84b84ffe1f9766fa8281694cb971
Ruben Bottu <ruben.bottu@qmino.com>
2023-10-30 11:08:43 +0100

    Small Cleanup

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -4,10 +4,6 @@ import { inject as service } from '@ember/service';
 export default class IndexRoute extends Route {
   @service store;
 
-  /* model() {
-    return this.store.findAll('artwork');
-  } */
-
   model() {
     return this.store.query('artwork', {
       include: 'artist',
