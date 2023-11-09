4e5286685532b7a6759847027ca2975d7fd3061e
Ruben Bottu <ruben.bottu@qmino.com>
2023-11-09 19:31:22 +0100

    Added recommendation endpoint

--- backend/config/dispatcher/dispatcher.ex
+++ backend/config/dispatcher/dispatcher.ex
@@ -55,6 +55,10 @@ defmodule Dispatcher do
     Proxy.forward(conn, path, "http://login/sessions/")
   end
 
+  match "/recommendations/*path" do
+    Proxy.forward(conn, path, "http://recommendation/artworks/")
+  end
+
   match "/*_", %{layer: :not_found} do
     send_resp(conn, 404, "Route not found.  See config/dispatcher.ex")
   end
--- backend/recommendation/app.js
+++ backend/recommendation/app.js
@@ -1,14 +1,78 @@
 import { app, query, errorHandler } from "mu";
 
-app.get("/hello", (req, res) => {
-    const query = `
+const executeQuery = async (queryString, next) => {
+    try {
+        return await query(queryString);
+    } catch (error) {
+        next(new Error(error));
+    }
+};
+
+const randomItem = (items) => {
+    return items[Math.floor(Math.random() * items.length)];
+};
+
+// id, type, title, art-form, description, image-url
+// artist: id, type, name
+app.get("/hello", async (req, res, next) => {
+    // TODO only pick artworks with art form most liked by logged in user
+    const getAllArtworksQuery = `
+        PREFIX schema: <http://schema.org/>
+        PREFIX foaf:   <http://xmlns.com/foaf/0.1/>
+        PREFIX mu:     <http://mu.semte.ch/vocabularies/core/>
+
         SELECT *
         WHERE {
-            ?artwork a <http://schema.org/VisualArtwork>;
-                <http://schema.org/headline> ?title
+            ?artwork a schema:VisualArtwork ;
+                mu:uuid ?artworkId ;
+                schema:headline ?title ;
+                schema:artform ?artForm ;
+                schema:abstract ?description ;
+                schema:image ?imageUrl ;
+                schema:artist ?artist .
+            ?artist mu:uuid ?artistId ;
+                schema:name ?name
         }
     `;
-    res.send("Hello mu-javascript-template");
+
+    const dbResponse = await executeQuery(getAllArtworksQuery, next);
+    const artworks = dbResponse.results.bindings;
+    const { artworkId, title, artForm, description, imageUrl, artistId, name } =
+        randomItem(artworks);
+
+    const body = {
+        data: [
+            {
+                type: "artworks",
+                id: artworkId.value,
+                attributes: {
+                    title: title.value,
+                    "art-form": artForm.value,
+                    description: description.value,
+                    "image-url": imageUrl.value,
+                },
+                relationships: {
+                    artist: {
+                        data: {
+                            type: "artists",
+                            id: artistId.value,
+                        },
+                    },
+                },
+            },
+        ],
+        included: [
+            {
+                type: "artists",
+                id: artistId.value,
+                attributes: {
+                    name: name.value,
+                },
+            },
+        ],
+    };
+
+    res.send(body);
 });
 
 app.use(errorHandler);
