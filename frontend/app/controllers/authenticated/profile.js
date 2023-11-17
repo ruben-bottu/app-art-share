176bb9dbef1d7e6d5aea6af6b8bf68757528ea09
Ruben Bottu <ruben.bottu@qmino.com>
2023-11-17 21:16:38 +0100

    Added mu-authorization

--- frontend/app/controllers/authenticated/profile.js
+++ frontend/app/controllers/authenticated/profile.js
@@ -4,64 +4,76 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class ProfileController extends Controller {
-  @tracked oldPassword = '';
-  @tracked newPassword = '';
-  @tracked newPasswordConfirmation = '';
+    @tracked oldPassword = '';
+    @tracked newPassword = '';
+    @tracked newPasswordConfirmation = '';
 
-  @service store;
-  @service router;
-  @service notify;
-  @service session;
+    @service store;
+    @service router;
+    @service notify;
+    @service session;
 
-  clearInputFields() {
-    this.oldPassword = '';
-    this.newPassword = '';
-    this.newPasswordConfirmation = '';
-  }
+    clearInputFields() {
+        this.oldPassword = '';
+        this.newPassword = '';
+        this.newPasswordConfirmation = '';
+    }
 
-  jsonApiHeaders() {
-    return {
-      Accept: 'application/vnd.api+json',
-      'Content-Type': 'application/vnd.api+json',
-    };
-  }
+    jsonApiHeaders() {
+        return {
+            Accept: 'application/vnd.api+json',
+            'Content-Type': 'application/vnd.api+json',
+        };
+    }
 
-  async updatePassword() {
-    const body = {
-      data: {
-        type: 'accounts',
-        id: 'current',
-        attributes: {
-          'old-password': this.oldPassword.trim(),
-          'new-password': this.newPassword.trim(),
-          'new-password-confirmation': this.newPasswordConfirmation.trim(),
-        },
-      },
-    };
+    async updatePassword() {
+        const body = {
+            data: {
+                type: 'accounts',
+                id: 'current',
+                attributes: {
+                    'old-password': this.oldPassword.trim(),
+                    'new-password': this.newPassword.trim(),
+                    'new-password-confirmation':
+                        this.newPasswordConfirmation.trim(),
+                },
+            },
+        };
 
-    return fetch('/accounts/current/changePassword', {
-      method: 'PATCH',
-      headers: this.jsonApiHeaders(),
-      body: JSON.stringify(body),
-    });
-  }
+        const response = await fetch('/accounts/current/changePassword', {
+            method: 'PATCH',
+            headers: this.jsonApiHeaders(),
+            body: JSON.stringify(body),
+        });
+        if (response.ok) {
+            this.notify.success(`Password changed succesfully`);
+        } else {
+            const error = await response.json();
+            if (error.errors && error.errors.length && error.errors[0].title) {
+                this.notify.error(error.errors[0].title);
+            } else {
+                if (error.status == 403) {
+                    this.notify.error(this.forbiddenMessage);
+                } else this.notify.error(this.failureMessage);
+            }
+        }
+    }
 
-  @action
-  async changePassword(event) {
-    event.preventDefault();
+    @action
+    async changePassword(event) {
+        event.preventDefault();
 
-    // TODO add validation
-    await this.updatePassword();
-    this.clearInputFields();
-    this.notify.success(`Password changed succesfully`);
-  }
+        // TODO add validation
+        await this.updatePassword();
+        this.clearInputFields();
+    }
 
-  @action
-  async unregister() {
-    this.session.invalidate();
-    await fetch('/accounts/current', {
-      method: 'DELETE',
-      headers: this.jsonApiHeaders(),
-    });
-  }
+    @action
+    async unregister() {
+        this.session.invalidate();
+        await fetch('/accounts/current', {
+            method: 'DELETE',
+            headers: this.jsonApiHeaders(),
+        });
+    }
 }
--- frontend/app/controllers/registration.js
+++ frontend/app/controllers/registration.js
@@ -4,41 +4,41 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class RegistrationController extends Controller {
-  @tracked name = '';
-  @tracked nickname = '';
-  @tracked password = '';
-  @tracked passwordConfirmation = '';
+    @tracked name = '';
+    @tracked nickname = '';
+    @tracked password = '';
+    @tracked passwordConfirmation = '';
 
-  @service store;
-  @service router;
-  @service notify;
+    @service store;
+    @service router;
+    @service notify;
 
-  clearInputFields() {
-    this.name = '';
-    this.nickname = '';
-    this.password = '';
-    this.passwordConfirmation = '';
-  }
+    clearInputFields() {
+        this.name = '';
+        this.nickname = '';
+        this.password = '';
+        this.passwordConfirmation = '';
+    }
 
-  async registerAccount() {
-    const account = this.store.createRecord('account', {
-      name: this.name.trim(),
-      nickname: this.nickname.trim(),
-      password: this.password.trim(),
-      passwordConfirmation: this.passwordConfirmation.trim(),
-    });
-    await account.save();
-    return account;
-  }
+    async registerAccount() {
+        const account = this.store.createRecord('account', {
+            name: this.name.trim(),
+            nickname: this.nickname.trim(),
+            password: this.password.trim(),
+            passwordConfirmation: this.passwordConfirmation.trim(),
+        });
+        await account.save();
+        return account;
+    }
 
-  @action
-  async register(event) {
-    event.preventDefault();
+    @action
+    async register(event) {
+        event.preventDefault();
 
-    // TODO add validation
-    const account = await this.registerAccount();
-    this.clearInputFields();
-    this.notify.success(`Registration successful`);
-    this.router.transitionTo('login');
-  }
+        // TODO add validation
+        const account = await this.registerAccount();
+        this.clearInputFields();
+        this.notify.success(`Registration successful`);
+        this.router.transitionTo('login');
+    }
 }
